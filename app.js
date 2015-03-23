e782285ea4d300d599df3a83600deaf29a863e9b
Daniel Leavitt <danleavitt0@gmail.com>
2015-03-23 10:27:22 -0700

    working gulp + react

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
     var express  = require('express');
-    var app = express();                               // create our app w/ express
-    var morgan = require('morgan');             // log requests to the console (express4)
-    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+    var http = require('http');
+    var app = express();                              
+    var morgan = require('morgan');
+    var bodyParser = require('body-parser');
+    var methodOverride = require('method-override');
     var request = require('request');
     var fs = require('fs');
 
     // configuration =================
 
-    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-    app.use(morgan('dev'));                                         // log every request to the console
-    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-    app.use(bodyParser.json());                                     // parse application/json
-    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+    app.use(express.static(__dirname + '/lib'));
+    app.use(morgan('dev'));
+    app.use(bodyParser.urlencoded({'extended':'true'}));            
+    app.use(bodyParser.json());
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
     app.use(methodOverride());
 
     app.post('*', function(req,res){
-      fs.readFile('./public/comments.json', function(err, data) {
-        console.log(data);
+      fs.readFile('./lib/' + req.url, function(err, data) {
         var comments = JSON.parse(data);
         comments.push(req.body);
-        fs.writeFile('./public/comments.json', JSON.stringify(comments, null, 4), function(err) {
+        fs.writeFile('./lib' + req.url, JSON.stringify(comments, null, 4), function(err) {
           res.setHeader('Content-Type', 'application/json');
           res.setHeader('Cache-Control', 'no-cache');
           res.send(JSON.stringify(comments));
         });
       });
+    });
+
+    app.get('/comments.json', function(req,res){
+      res.sendfile('./lib/' + req.url);
     });
 
     app.get('*', function(req, res) {
@@ -34,4 +38,10 @@
 
 
     // listen (start app with node server.js) ======================================
-    app.listen(process.env.PORT || 3000);
+    app.set('port', process.env.PORT || 5000);
+
+
+    http.createServer(app).listen(app.get('port'), function(){
+      console.log("Express server listening on port " + app.get('port'));
+      process.send && process.send('listening');
+    });
